d361a0ff80fe61a6c0a166b4a6430b341221a867
OHusan <omerinovic27@gmail.com>
2024-11-18 16:55:25 +0100

    fix: issue with addToCart

--- resources/js/Pages/User/Layouts/Products.vue
+++ resources/js/Pages/User/Layouts/Products.vue
@@ -1,26 +1,9 @@
 <script setup>
-import { Link, router } from '@inertiajs/vue3'
+import { Link } from '@inertiajs/vue3'
 import ProductDisplay from '../Components/ProductDisplay.vue';
 defineProps({
     products: Array
 })
-
-const addToCart = (product) => {
-    console.log(product)
-    router.post(route('cart.store', product), {
-        onSuccess: (page) => {
-            if (page.props.flash.success) {
-                Swal.fire({
-                    toast: true,
-                    icon: 'success',
-                    position: 'top-end',
-                    showConfirmButton: false,
-                    title: page.props.flash.success
-                });
-            }
-        }
-    })
-}
 </script>
 
 <template>
@@ -28,11 +11,12 @@ const addToCart = (product) => {
         <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
             <h2 class="text-2xl font-bold tracking-tight text-gray-900">List of products</h2>
 
-            <ProductDisplay :products="products"/>
+            <ProductDisplay :products="products" />
 
             <div class="flex justify-center mt-5">
                 <Link :href="route('products.index')"
-                class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Show all products</Link>
+                    class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                Show all products</Link>
             </div>
         </div>
     </div>
